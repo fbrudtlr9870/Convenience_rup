dae688a25e943fd3fcea54f06df1097a064820d6
Hyunbun <hyc08@hotmail.com>
2018-06-14 13:24:07 +0900

    this is a test

---
+++ Convenience_rup/testing.java
@@ -0,0 +1,12 @@
+package test;
+
+public class testing {
+	public static void main(String[] args) {
+		int i = 0;
+		int j = 1;
+		
+		System.out.println(i+j);
+		System.out.println("hello world!");
+		System.out.println("this is a test");
+	}
+}
