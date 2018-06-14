ca48a37b46ea6d9a57b63d99891561abe7552368
jackalpsh <tkdgus2918@naver.com>
2018-06-14 14:38:03 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'soon' of
    https://github.com/fbrudtlr9870/Convenience_rup.git into sang
    
    Conflicts:
    	Convenience_rup/src/main/java/com/proj/rup/HomeController.java
    	Convenience_rup/src/main/webapp/WEB-INF/views/home.jsp
    	Convenience_rup/target/m2e-wtp/web-resources/META-INF/maven/com.final/rup/pom.properties

--- Convenience_rup/src/main/java/com/proj/rup/HomeController.java
+++ Convenience_rup/src/main/java/com/proj/rup/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
- * 수정했지요
+ * 수정했지요l,l
  */
 @Controller
 public class HomeController {
